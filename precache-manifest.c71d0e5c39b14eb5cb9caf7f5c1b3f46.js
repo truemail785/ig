d0933589a02a43ddc49615004ef40f3ab9201ed2
ozair <truemail785@gmail.com>
2020-09-15 22:36:09 +0500

    Updates

---
+++ precache-manifest.c71d0e5c39b14eb5cb9caf7f5c1b3f46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d8053f44a861a9efcfd684b6c8474e",
+    "url": "/ig/index.html"
+  },
+  {
+    "revision": "73ca5ba39f9fd46c3cec",
+    "url": "/ig/static/css/main.84a58ffe.chunk.css"
+  },
+  {
+    "revision": "48a5c3901b682e9abbed",
+    "url": "/ig/static/js/2.78f09901.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/ig/static/js/2.78f09901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73ca5ba39f9fd46c3cec",
+    "url": "/ig/static/js/main.2433556c.chunk.js"
+  },
+  {
+    "revision": "47f4ebdaf2bf13c20bc1",
+    "url": "/ig/static/js/runtime-main.8ab89a7f.js"
+  }
+]);
