6ed929c5dd6f05f4c22cc0c6578773f3875de1b4
ozair <truemail785@gmail.com>
2020-09-18 01:16:30 +0500

    Updates

---
+++ precache-manifest.3cb1cd7afd836747e009d22cc769e846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261a9fc8b7e9c18bb093d9a8ea298cd4",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "92b2c6b78f82cf8c2258",
+    "url": "/react-gh-pages/static/css/main.9e81c3f1.chunk.css"
+  },
+  {
+    "revision": "0c5e8bb4f971352d01de",
+    "url": "/react-gh-pages/static/js/2.6159594f.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/react-gh-pages/static/js/2.6159594f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b2c6b78f82cf8c2258",
+    "url": "/react-gh-pages/static/js/main.234c7707.chunk.js"
+  },
+  {
+    "revision": "4ef1dbff6c2117628797",
+    "url": "/react-gh-pages/static/js/runtime-main.ee92d99d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ig/precache-manifest.41f4cad643b6cd46ffbe567cea16fb32.js"
+  "/react-gh-pages/precache-manifest.3cb1cd7afd836747e009d22cc769e846.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ig/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
